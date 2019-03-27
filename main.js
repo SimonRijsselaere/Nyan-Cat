3c775921ef34d2febedcf7fe4145b2b9e1db387a
SimonRijsselaere <rijsselaere.simon@gmail.com>
2019-03-27 17:02:45 +0100

    let nyan move

--- main.js
+++ main.js
@@ -1,43 +1,20 @@
 var nyan = document.getElementById("nyan");
-var backImage = document.createElement("img");
+/*var backImage = document.createElement("img");*/
 
-nyan.addEventListener("click", doAll);
+nyan.addEventListener("click", addNyan);
 
 function addNyan() {
-  var image = document.createElement("img");
-  image.src = "nyanCat.gif";
-  image.id = "kat";
-  image.classList.add('nyanCat');
-  image.classList.add('nyanCat-start');
-  image.style.height = '60px';
-  document.body.appendChild(image);
+  var imageNyan = document.createElement("img");
+  imageNyan.src = "nyanCat.gif";
+  document.body.appendChild(imageNyan);
+
+  imageNyan.classList.add('nyan-cat-start');
+  imageNyan.classList.add('nyan-cat');
+
   document.body.style.backgroundImage = "url('nyanCatBackground.gif')";
   document.body.style.backgroundSize = '30vh';
 
-
+  setTimeout( function () {
+    imageNyan.classList.add('nyan-cat-end');
+  }, 10);
 }
-
-function moveNyan() {
-
-  document.getElementById('kat').style.animation = 'mymove 2s infinite';
-
-}
-
-
-function doAll() {
-  addNyan();
-  moveNyan();
-}
-
-/*
-function endNyanImage(){
-  document.getElementById("kat").className = "nyan-cat-end";
-}
-
-
-/*
-function doAll() {
-  addNyanImage();
-  setTimeout(endNyanImage(), 3000);
-}
-*/
