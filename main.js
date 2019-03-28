bf2bddd4af55d2d82bc10785fb3d25c586d9e19b
SimonRijsselaere <rijsselaere.simon@gmail.com>
2019-03-28 10:43:17 +0100

    made it work, disabled audio

--- main.js
+++ main.js
@@ -1,20 +1,50 @@
 var nyan = document.getElementById("nyan");
 /*var backImage = document.createElement("img");*/
+var x = document.getElementById("fuckingWeebMusic");
+
+
+function playAudio() {
+  x.play();
+}
+
+var music = setInterval(playAudio, 5000);
+setTimeout(function( ) {
+  clearInterval(music);
+}, 5000);
+
 
 nyan.addEventListener("click", addNyan);
 
 function addNyan() {
-  var imageNyan = document.createElement("img");
-  imageNyan.src = "nyanCat.gif";
-  document.body.appendChild(imageNyan);
 
-  imageNyan.classList.add('nyan-cat-start');
-  imageNyan.classList.add('nyan-cat');
+  var myVar = setInterval(myTimer, 1000);
 
-  document.body.style.backgroundImage = "url('nyanCatBackground.gif')";
-  document.body.style.backgroundSize = '30vh';
 
-  setTimeout( function () {
-    imageNyan.classList.add('nyan-cat-end');
-  }, 10);
+  function myTimer(){
+      var imageNyan = document.createElement("img");
+      imageNyan.src = "nyanCat.gif";
+      document.body.appendChild(imageNyan);
+
+
+      imageNyan.classList.add('nyan-cat-start');
+      imageNyan.classList.add('nyan-cat');
+
+      document.body.style.backgroundImage = "url('nyanCatBackground.gif')";
+      document.body.style.backgroundSize = '30vh';
+      nyan.style.display = "none";
+
+      setTimeout( function () {
+        imageNyan.classList.add('nyan-cat-end');
+      }, 10);
+
+
+  }
+
+  setTimeout(function( ) {
+    clearInterval(myVar);
+    document.body.style.backgroundImage = "";
+    nyan.style.display = "block";
+  }, 5000);
+
+
 }
