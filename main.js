9b1fc445828c75f079513537f175a4a9beb91b61
SimonRijsselaere <rijsselaere.simon@gmail.com>
2019-03-29 12:10:11 +0100

    add random, enabled kutsound

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 var nyan = document.getElementById("nyan");
-/*var backImage = document.createElement("img");*/
 var x = document.getElementById("fuckingWeebMusic");
 
 
@@ -13,42 +12,40 @@ setTimeout(function( ) {
 }, 5000);
 
 
-nyan.addEventListener("click", addNyan);
+nyan.addEventListener("click", function(){
+  addNyan();
+  var myVar = setInterval(addNyan, 70);
+  setTimeout(function( ) {
+    clearInterval(myVar);
+    document.body.style.backgroundImage = "";
+    nyan.style.display = "block";
+    }, 10000);
+});
 
 function addNyan() {
-
-  var myVar = setInterval(myTimer, 100);
-
-
-  function myTimer(){
       var imageNyan = document.createElement("img");
       imageNyan.src = "nyanCat.gif";
-      document.body.appendChild(imageNyan);
 
 
-      imageNyan.classList.add('nyan-cat-start');
-      imageNyan.classList.add('nyan-cat');
-
-      var random = document.getElementsByClassName("nyan-cat");
-
+      imageNyan.style.position = "fixed";
+      imageNyan.style.left ='-'+ Math.floor(Math.random()*(11-1))+1+'%';
+      imageNyan.style.top='0%';
+      imageNyan.style.transition = Math.floor(Math.random()*(6-2))+2 +'s';
+      imageNyan.style.transform = "rotate("+Math.floor(Math.random()*(180-90)+90) +'deg)';
+      imageNyan.style.height = "55px";
 
 
       document.body.style.backgroundImage = "url('nyanCatBackground.gif')";
       document.body.style.backgroundSize = '30vh';
       nyan.style.display = "none";
 
+
+      document.body.appendChild(imageNyan);
       setTimeout( function () {
         imageNyan.classList.add('nyan-cat-end');
+        imageNyan.style.left='100%';
+        imageNyan.style.top='100%';
+        imageNyan.style.transform = "rotate("+Math.random()*((360-0)+0)+'deg)';
+
       }, 10);
-
-
-  }
-
-  setTimeout(function( ) {
-    clearInterval(myVar);
-    document.body.style.backgroundImage = "";
-    nyan.style.display = "block";
-  }, 10000);
-
-
 }
