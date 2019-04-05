2e1523122486312265ff955356bccd5218dcf27a
SimonRijsselaere <rijsselaere.simon@gmail.com>
2019-04-05 15:59:56 +0200

    removing images after 10 seconds

--- main.js
+++ main.js
@@ -1,44 +1,41 @@
 var nyan = document.getElementById("nyan");
 var x = document.getElementById("fuckingWeebMusic");
 
-
-function playAudio() {
-  x.play();
-}
-
-var music = setInterval(playAudio, 5000);
-setTimeout(function( ) {
-  clearInterval(music);
-}, 5000);
-
-
 nyan.addEventListener("click", function(){
   addNyan();
+  function playAudio() {
+    x.play();
+  }
+  var music = setInterval(playAudio, );
+  setTimeout(function( ) {
+    clearInterval(music);
+  }, 10);
   var myVar = setInterval(addNyan, 70);
   setTimeout(function( ) {
     clearInterval(myVar);
     document.body.style.backgroundImage = "";
     nyan.style.display = "block";
+    var imageNyan = document.getElementsByClassName('nyan-cat-end');
+    console.log(imageNyan);
+    var counter = 0;
+    while(imageNyan.length){
+      imageNyan[counter].parentNode.removeChild(imageNyan[counter])
+    }
     }, 10000);
 });
 
 function addNyan() {
       var imageNyan = document.createElement("img");
       imageNyan.src = "nyanCat.gif";
-
-
       imageNyan.style.position = "fixed";
       imageNyan.style.left ='-'+ Math.floor(Math.random()*(11-1))+1+'%';
       imageNyan.style.top='0%';
       imageNyan.style.transition = Math.floor(Math.random()*(6-2))+2 +'s';
       imageNyan.style.transform = "rotate("+Math.floor(Math.random()*(180-90)+90) +'deg)';
       imageNyan.style.height = "55px";
-
-
       document.body.style.backgroundImage = "url('nyanCatBackground.gif')";
       document.body.style.backgroundSize = '30vh';
       nyan.style.display = "none";
-
 
       document.body.appendChild(imageNyan);
       setTimeout( function () {
@@ -46,6 +43,5 @@ function addNyan() {
         imageNyan.style.left='100%';
         imageNyan.style.top='100%';
         imageNyan.style.transform = "rotate("+Math.random()*((360-0)+0)+'deg)';
-
       }, 10);
 }
